73ae3bf2f6a50bb456ae96a8055aadd026ae3fec
Jonathan Fraimorice <yonifra@gmail.com>
2016-04-25 14:18:02 +0300

    Added some unit testing

---
+++ SEMJournals.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SEMJournals.Server;
+
+namespace SEMJournals.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var service = new Service1();
+            var journals = service.GetAllJournals();
+
+            Assert.IsNotNull(journals);
+        }
+    }
+}
