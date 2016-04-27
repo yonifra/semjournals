5d7db6b32d46f6967dafb72bfe03048e7003819a
Jonathan Fraimorice <yonifra@gmail.com>
2016-04-27 12:35:23 +0300

    Added EF to the Windows solution + More methods in the UsersManager class

--- SEMJournals.Common/Models/UsersManager.cs
+++ SEMJournals.Common/Models/UsersManager.cs
@@ -7,9 +7,17 @@ namespace SEMJournals.Common.Models
     {
         public static void AddUser(string username, UserType type)
         {
+            AddUser(new User(username) { Journals = new List<Journal>(), Type = type });
+        }
+
+
+        public static void AddUser(User user)
+        {
+            if (user == null) return;
+
             using (var db = new JournalsContext())
             {
-                db.Users.Add(new User(username) { Journals = new List<Journal>(), Type = type });
+                db.Users.Add(user);
                 db.SaveChanges();
             }
         }
@@ -22,19 +30,32 @@ namespace SEMJournals.Common.Models
 
                 if (userToDelete != null)
                 {
-                    db.Users.Remove(userToDelete);
-                    db.SaveChanges();
+                    RemoveUser(userToDelete);
                 }
+            }
+        }
+
+        public static void RemoveUser(User user)
+        {
+            if (user == null) return;
+
+            using (var db = new JournalsContext())
+            {
+                db.Users.Remove(user);
+                db.SaveChanges();
             }
         }
 
         public static User GetUserByUsername(string username)
         {
-            User foundUser;
+            User foundUser = null;
 
             using (var db = new JournalsContext())
             {
-                foundUser = db.Users.FirstOrDefault(u => u.Username == username);  
+                if (db.Users.Any())
+                {
+                    foundUser = db.Users.FirstOrDefault(u => u.Username == username);
+                }
             }
 
             return foundUser;
